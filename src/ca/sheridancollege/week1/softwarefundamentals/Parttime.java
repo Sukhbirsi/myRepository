03346891debf50e9593ec5feabebc6b3867130db
sukhbirsi <sukhbirdeol6@gmail.com>
2019-05-21 13:11:59 -0400

    code for student

---
+++ src/ca/sheridancollege/week1/softwarefundamentals/Parttime.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.week1.softwarefundamentals;
+
+/**
+ *
+ * @author sukhb
+ */
+public class Parttime {
+    private String noClasses;
+    
+    
+    public String getNoClasses(){
+        return noClasses;
+    
+    }
+    
+    public void setNoClasses(String noClasses){
+        this.noClasses = noClasses;
+    }
+}
